0eff9523daa48c8a6d2e8f65a73632bbaffa4e34
TIGER-yassine <yassinesallemi1337@gmail.com>
2023-11-06 14:46:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { player } from "./Data";
+import PlayerList from "./components/PlayerList";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const playerinfo = player;
+  const showBd = (name, number) => {
+    alert(`${name} has ${number} ballon d'ors`);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlayerList playerinfo={playerinfo} showBd={showBd} />
     </div>
   );
 }
